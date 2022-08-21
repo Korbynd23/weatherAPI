6778e35bacd1ad2169895f740d906774595358c6
kdicari24@gmail.com <kdicari24@gmail.com>
2022-08-21 12:33:13 -0400

    added jumbotron and search form

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+function getWeatherApi() {
+    let requestUrl = 'https://api.openweathermap.org/data/2.5/forecast?q={city name}&units=imperial&appid=01c6acda042379425ee30a68789c29c5'
+    //create a var param for input city^^
+    
+}
